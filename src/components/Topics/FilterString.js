a66550d81636148921dcf38f9803b54d2384c4d2
Nathan Klundt <nathanryan001@gmail.com>
2017-10-29 07:42:02 -0600

    Topic Browser connected with Topics, basic structure set up

---
+++ src/components/Topics/FilterString.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class FilterString extends Component {
+    render(){
+        return(
+            <h4>Filter String</h4>
+        )
+    }
+}
---
+++ src/components/Topics/Sum.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Sum extends Component {
+    render(){
+        return(
+            <h4>Sum</h4>
+        )
+    }
+}
